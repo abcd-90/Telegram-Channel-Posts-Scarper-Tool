e1994a51b28499058851d5f19d386ecb652ff65c
Laptop Mart <githubio562@gmail.com>
2026-07-31 13:20:08 -0700

    Fix cloud HTTP MTProto connection for PythonAnywhere

--- src/run_userbot_cloner.py
+++ src/run_userbot_cloner.py
@@ -30,11 +30,13 @@ async def main():
     db = Database("data/clone_history.db")
     await db.init_db()
 
+    from telethon.network import ConnectionTcpHttp
     session_path = "data/user_session"
     client = TelegramClient(
         session_path,
         config.telegram.api_id,
         config.telegram.api_hash,
+        connection=ConnectionTcpHttp,
         sequential_updates=True
     )
 
